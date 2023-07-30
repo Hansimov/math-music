9f1ec693787d1f5e437eb3d803d6af6372e5bf4e
Hansimov <591172499@qq.com>
2023-07-30 18:39:07 +0800

    [Feature] Create dots

--- speed_transform.py
+++ speed_transform.py
@@ -7,12 +7,13 @@ def create_glow_for_dot(
     circles_num=60,
     glow_radius_ratio=10,
     color=None,
-    decay_func=lambda i: 0.8 / (pow(i, 1) + 0.001),
+    decay_func=lambda i: 0.8 / (pow(i, 1.2) + 0.001),
 ):
     glow = VGroup()
     max_glow_radius = dot.radius * glow_radius_ratio
     for i in range(circles_num):
         circle = Circle(
+            arc_center=dot.get_center(),
             radius=i / circles_num * max_glow_radius,
             fill_opacity=decay_func(i),
             fill_color=dot.color if color is None else color,
@@ -22,21 +23,36 @@ def create_glow_for_dot(
     return glow
 
 
-class GlowingDot(VGroup):
-    def __init__(self, **kwargs):
+class GlowDot(VGroup):
+    def __init__(
+        self,
+        dot,
+        **kwargs,
+    ):
         VGroup.__init__(self, **kwargs)
-        self.dot = Dot3D(color=YELLOW)
-        self.add(self.dot)
-        self.glow = create_glow_for_dot(self.dot)
-        self.add(self.glow)
+        self.dot = dot
+        self.glow = create_glow_for_dot(dot)
+        self.add(self.dot, self.glow)
 
 
 class SpeedTransform(ThreeDScene):
+    def create_dots(self):
+        for i in range(5):
+            fadein_time = 2 - i / 5
+            dot = Dot3D(color=YELLOW, point=[i, 0, 0])
+            glow_dot = GlowDot(dot)
+            self.play(FadeIn(glow_dot), run_time=fadein_time)
+            # self.wait()
+
+    def set_camera_options(self):
+        self.set_camera_orientation(
+            phi=75 * DEGREES,
+            theta=30 * DEGREES,
+        )
+
     def construct(self):
-        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        glowing_dot = GlowingDot()
-        self.play(FadeIn(glowing_dot))
-        self.wait()
+        self.set_camera_options()
+        self.create_dots()
 
 
 def main():
