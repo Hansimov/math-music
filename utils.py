2eb3114bc58275a93072ca06df7f59fa19b6e20e
Hansimov <591172499@qq.com>
2023-07-30 20:54:13 +0800

    [Fix] Divided by zero when len(objs) is 0

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from manim import *
+import numpy as np
 
 
 def calc_centroid(objs):
     """calculates the centroid of objects"""
+    if len(objs) == 0:
+        return np.array([0, 0, 0])
     return sum([obj.get_center() for obj in objs]) / len(objs)
